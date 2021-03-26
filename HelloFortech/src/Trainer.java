f12a366351612209129c84f58ec847c04b2156ac
alexb1298 <81372050+alexb1298@users.noreply.github.com>
2021-03-26 11:28:12 +0200

    Update the Trainer class from github
    Added the fields name,age and role as well their setters/getters

--- HelloFortech/src/Trainer.java
+++ HelloFortech/src/Trainer.java
@@ -1,2 +1,29 @@
-public class Trainer {
+public class Trainer 
+{
+    private String name;
+    private int age;
+    private String role;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+     public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
